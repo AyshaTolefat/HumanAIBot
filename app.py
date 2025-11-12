2508683000c4cd9c84cd069eb31e55562e411f47
Aysha Tolefat <aysha.tolefat@kcl.ac.uk>
2025-11-12 13:31:50 +0000

    Initial app

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Welcome to your own Tutor Bot!")
+st.markdown(
+    """
+    This bot helps you understand your strengths and weaknesses in any topic you would like. 
+
+    Start by giving a topic, or uploading a document to recieve some questions and identify your strengths and weaknesses.
+    """
+)
